ac31fab02a1d5a7b2a74b6a69dff521fb26ed427
Alex <pr1me-gop@yandex.ru>
2019-11-25 22:59:33 +0400

    форма с валидацией телефона (full)

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,37 @@
 import IMask from 'imask';
 
 window.onload = function () {
-  const form = document.querySelector('.form').addEventListener('submit', function (e) {
-    e.preventDefault();
+
+  /* Валидация ввода телефона */
+  const telRegExp = /\+7\s?[ ][\(]{0,1}9[0-9]{2}[\)][ ]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}/;
+  const form = document.querySelector('.form');
+  const telInput = document.querySelector('.form-tel-input');
+  const telError = document.querySelector('.form-tel-error');
+
+  telInput.addEventListener('input', function () {
+    const text = telInput.value.length === 0 || telRegExp.test(telInput.value);
+    console.log(text);
+    if (text) {
+      telInput.classList.remove('error-input');
+    } else {
+      telInput.classList.add('error-input');
+    }
   });
+
+  form.addEventListener('submit', function (event) {
+    const text = telInput.value.length !== 0 && telRegExp.test(telInput.value);
+    console.log(text);
+    if (text) {
+      telInput.classList.remove('error-input');
+      telError.classList.remove('error-text');
+    } else {
+      telInput.classList.add('error-input');
+      telError.classList.add('error-text');
+      event.preventDefault();
+    }
+  });
+
+  /* Телефонная маска */
 
   IMask(document.querySelector('.form-tel-input'), {
     mask: [
