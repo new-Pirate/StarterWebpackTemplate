41e69b7781c1c8d50865025067601b7c9677c503
Alex <pr1me-gop@yandex.ru>
2019-12-01 16:21:25 +0400

    add @babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 const config = {
   entry: ["./src/js/index.js", "./src/scss/style.scss"],
@@ -16,6 +15,16 @@ const config = {
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       {
         test: /\.(sass|scss)$/,
         include: path.resolve(__dirname, "src/scss"),
